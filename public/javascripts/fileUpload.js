976f1511091d1e133a4673689b9ba13ade1244f6
Steve Johnstone <t-sjohnstone@hotels.com>
2021-04-26 15:28:34 +0100

    styling finalised

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,32 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode
-)
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
+const rootStyles = window.getComputedStyle(document.documentElement);
 
-FilePond.parse(document.body);
+if (rootStyles.getPropertyValue('--book-cover-width-large') != null &&
+    rootStyles.getPropertyValue('--book-cover-width-large') != '') {
+    ready();
+} else {
+    document.getElementById('main-css')
+    .addEventListener('load', ready)
+}
+
+function ready () {
+
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'));
+    const coverAspectRato = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'));
+    const coverHeight = coverWidth / coverAspectRato;
+
+
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode
+    )
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 /coverAspectRato,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    })
+    FilePond.parse(document.body);
+}
+
+
